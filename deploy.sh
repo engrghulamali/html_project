56cfc65af927a4f5cb1c091d1a1593258f3ab027
Ghulam Ali <44402785+engrghulamali@users.noreply.github.com>
2024-09-05 01:44:10 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Update HTML files from S3
-aws s3 cp s3://my-html-project-bucket/index.html /var/www/html/index.html
+aws s3 cp s3://codepipeline-ap-south-1-750088031045/index.html /var/www/html/index.html
